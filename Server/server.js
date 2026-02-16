1d0b18f851c290ce01b1fb775b318b5fd48a57d6
Kishore M <m.kishore732006mahes@gmail.com>
2026-02-16 13:59:58 +0530

    Setup1

---
+++ Server/server.js
@@ -0,0 +1,24 @@
+const dns = require("dns");
+dns.setServers(["1.1.1.1"]);
+
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
